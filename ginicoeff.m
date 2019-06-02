bfab2af771a9dfe9665ae7d34f0bba3d7e375d57
anyahui <anyahui.120@gmail.com>
2019-06-02 13:49:26 +0800

    Update ginicoeff.m

--- ginicoeff.m
+++ ginicoeff.m
@@ -1,31 +1,25 @@
-function [coeff] = ginicoeff(pred,K)
+function [ gini_matrix ] = giniCoeff( train_data)
+%GINICOFF Summary of this function goes here
+%train_data is a m*n matrix where each row is for a user and the value 0/1 for each element represents selected/unselected.
     %sort pred
-    [m,n] = size(pred);
-    [~,sat] = sort(pred,2);
-    rec_matrix = zeros(m,n);
+    [m,~] = size(train_data);
+    train_col_du = sum(train_data,1); % popularity of items
+    train_row_du = sum(train_data,2); % activity of users
+    train_col_du_matrix = repmat(train_col_du,[m,1]);
+    user_item_degree_matrix = train_data .* train_col_du_matrix;
+    clear train_data train_col_du train_col_du_matrix;
+    gini_matrix = zeros(m,1); 
     for i = 1:m
-        temp = sat(i, n - K + 1 : n);
-        rec_matrix(i, temp) = 1;
+        item_degrees = user_item_degree_matrix(i,find(user_item_degree_matrix(i,:)~= 0));
+        [A,~] = sort(item_degrees,2);
+        [freq, diff_degrees] = hist(A,unique(A));
+        gini = 0; n = length(A);
+        for rank = 1:1:n
+            item_degree = A(rank);
+            item_count = freq(find(diff_degrees == item_degree));
+            gini = gini + (n+1-rank)/(n+1) * (item_count/n);
+        end
+        gini_matrix(i) = gini; 
     end
-    %popularity for every item
-    In(1,:) = sum(rec_matrix, 1);
-    if isvector(In); In = In(:); dim = 1; end
-    % Sort In
-    In = sort(In,dim,'ascend');
-
-    N = length(In);
-    % Totals
-    tot = sum(In,dim);
-
-    % Gini's coefficient
-    % coeff = N+1-2*(sum(In.*freq,dim)./tot);
-    coeff = 0;
-    for i = 1:N
-        G = (2*i - N -1) .* (In(i,1) ./ tot);
-        coeff = coeff + G; 
-    end
-    % Sample correction
-    coeff = coeff./(N-1);
 end
-
 
